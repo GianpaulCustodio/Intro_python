2b8d78a385ac1aa31d632c90a75e776960dd8f44
Gpspidey19 <gianpaulcustodio1198@gmail.com>
2021-02-15 12:43:55 -0500

    Cambios1

---
+++ Holamundo.py
@@ -0,0 +1,42 @@
+#ejercicio1 For
+#coleccion = {"Gp":22,"Spiderman":20,"Avengers":13}
+#for clave,valor in coleccion.items():
+#    print(f"{clave} -> {valor}")
+
+
+#Ejercicio2-Intro
+# a = int(input("Ingrese a->"))
+# b = int(input("Ingrese b->"))
+# suma=a+b
+# print(f"La respuesta es {suma}")
+
+
+#Intercambiar el valor entre 2 variables
+# a = int(input("Ingrese a->"))
+# b = int(input("Ingrese b->"))
+# a,b=b,a
+# print(f"valor de a-> {a}")
+# print(f"valor de b-> {b}")
+
+
+#Intercambiar el valor entre 3 variables
+# a = int(input("Ingrese a->"))
+# b = int(input("Ingrese b->"))
+# c= int(input("Ingrese c->"))
+# a,b,c=c,a,b
+# print(f"valor de a-> {c}")
+# print(f"valor de b-> {a}")
+# print(f"valor de c-> {b}")
+
+
+#Radio de circunferencia
+# import math
+# radio = float(input("Ingrese r ->"))
+# longitud=2*math.pi*radio
+# area=math.pi*radio*radio
+# print(f"longitud -> {longitud}")
+# print(f"area -> {area}")
+# print("{0:.2f}".format(longitud)) #Para dos decimales
+##aaa
+##bbb
+
