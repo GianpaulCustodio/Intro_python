149ff6d9dc88d6238f9be14551226b334cac1832
Gpspidey19 <gianpaulcustodio1198@gmail.com>
2021-02-15 13:28:51 -0500

    Cambios 5

--- Holamundo.py
+++ Holamundo.py
@@ -37,11 +37,29 @@
 # print(f"longitud -> {longitud}")
 # print(f"area -> {area}")
 # print("{0:.2f}".format(longitud)) #Para dos decimales
-##aaa
-##bbb
-##ccc
-##ddd
-##eee
-##fff
-##ggg
+
+
+# #condicionales
+# numero = int(input("ingrese un numero ->"))
+# if numero>0:
+#     print("Numero positivo")
+# elif numero==0:
+#     print("Numero es cero")
+# else:
+#     print("Numero negativo")
+
+
+##condicionales multiples
+# numero=int(input("Ingresa tu edad ->"))
+# if numero>0:
+#     if numero>=18 and numero<60:
+#         input("Es mayor de edad")
+#     elif numero>=60:
+#         input("Es viejo")
+#     else:
+#         input("Es menor de edad")
+# elif numero==0:
+#     print("Numero es cero")
+# else:
+#     input("El numero debe ser positivo")
 
