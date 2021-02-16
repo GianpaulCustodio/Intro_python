fcadcd997c6470b3c890fd0a0a300dd5916a270c
Gpspidey19 <gianpaulcustodio1198@gmail.com>
2021-02-15 19:04:33 -0500

    Cambios 7

--- Holamundo.py
+++ Holamundo.py
@@ -78,3 +78,11 @@
 #       print("Ningún número es par")
 
 
+# #Determinar si es una vocal o no
+# letra=input("Ingrese una letra: ").lower() #Transforma la mayuscula a minuscula
+# if letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u':
+#     print("Es una vocal")
+# else:
+#     print("NO es una vocal")
+
+
